b8be4d50463be11c712f5455678e955bed112823
medicharla2 <98125181+medicharla2@users.noreply.github.com>
2023-03-26 23:49:30 -0400

    working color detection

--- test.py
+++ test.py
@@ -1,7 +1,37 @@
-from transformers import pipeline
 import cv2
-from PIL import Image 
-from sentence_transformers import SentenceTransformer, util
+from PIL import Image
 
-image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+cap = cv2.VideoCapture("video_file.mp4")
+
+if cap.isOpened() == False:
+    print('error opening file image classification')
+
+(success, image) = cap.read()
+
+list = []
+
+while success:
+    pil_image = Image.fromarray(image)
+
+    res = pil_image.getcolors(pil_image.size[0] * pil_image.size[1])
+
+    sorted_colors = sorted(res, key = lambda x:x[0], reverse=True)
+
+    top_colors = sorted_colors[:3]
+
+    rgb_colors = [c[1] for c in top_colors]
+
+    if rgb_colors[0] not in list:
+        list.append(rgb_colors[0])
+    if rgb_colors[1] not in list:
+        list.append(rgb_colors[1])
+    if rgb_colors[2] not in list:
+        list.append(rgb_colors[2])
+
+    key = cv2.waitKey(1)
+
+    (success, image) = cap.read()
+cv2.destroyAllWindows()
+
+print(list)
 
