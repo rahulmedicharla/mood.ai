e1b5074e0b6c58acc7601643377559c13092b3f7
medicharla2 <98125181+medicharla2@users.noreply.github.com>
2023-05-17 15:06:28 -0400

    test for valid link and openai key before running

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import analysis_parent
+import openai
 
 #flask api initializer
 app = Flask(__name__)
@@ -12,10 +13,26 @@ CORS(app)
 class MoodAi(Resource):
     #entry point for endpoint
     def get(self, video_link, openaikey):
+        
+        data = {
+            "image_results": [],
+            "error": ""
+        }
 
-        image_results = analysis_parent.main(video_link, openaikey)
+        try:
+            openai.api_key = openaikey
+            openai.Completion.create(
+                engine='davinci',
+                prompt='test openai key',
+                max_tokens = 5
+            )
 
-        return image_results
+            data["image_results"] = analysis_parent.main(video_link, openaikey)
+        except (Exception, OSError, TypeError, openai.error.AuthenticationError)  as e:
+            data["image_results"] = None
+            data["error"] = str(e)
+
+        return data
     
 api.add_resource(MoodAi, '/moodai/<path:video_link>/<string:openaikey>')
 
--- test_api.py
+++ test_api.py
@@ -1,10 +1,9 @@
 import requests
 from keys import openaikey
-import urllib.parse
 
 BASE = "http://127.0.0.1:5000/"
 
-url = "https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fmood-ai-34418.appspot.com%2Fo%2Frecordings%252FZ94e4nOz89NXtVAL8YwaWTK2Edl1.mp4%3Falt%3Dmedia%26token%3D0dbd2a89-552e-48c4-bd34-dc0d9c4136be"
+url = "https%3A%2F%2Ffirebasestorb%2Fmood-ai-34418.appspot.com%2Fo%2Frecordings%252FZ94e4nOz89NXtVAL8YwaWTK2Edl1.mp4%3Falt%3Dmedia%26token%3D0dbd2a89-552e-48c4-bd34-dc0d9c4136be"
 
 final = BASE + 'moodai/' + url +  '/' + openaikey
 response = requests.get(final)
