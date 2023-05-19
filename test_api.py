e1ea3f783b1535c57e90184fed7e7708959a260f
rahulmedicharla <46610295+rahulmedicharla@users.noreply.github.com>
2023-05-19 13:51:52 -0400

    updated public test_api

--- test_api.py
+++ test_api.py
@@ -1,9 +1,9 @@
 import requests
-from keys import openaikey
 
 BASE = "http://127.0.0.1:5000/"
 
-url = "https%3A%2F%2Ffirebasestorb%2Fmood-ai-34418.appspot.com%2Fo%2Frecordings%252FZ94e4nOz89NXtVAL8YwaWTK2Edl1.mp4%3Falt%3Dmedia%26token%3D0dbd2a89-552e-48c4-bd34-dc0d9c4136be"
+openaikey = "ENTER_OPENAI_KEY"
+url = "ENTER_VIDEO_URL"
 
 final = BASE + 'moodai/' + url +  '/' + openaikey
 response = requests.get(final)
